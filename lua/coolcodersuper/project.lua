0db7e0563367d33e392c7bc18078d50e7963286d
Joseph Guenther <joseph@codingcool.ca>
2025-04-04 14:00:07 -0400

    Improve terminal

--- lua/coolcodersuper/project.lua
+++ lua/coolcodersuper/project.lua
@@ -1,5 +1,9 @@
 local root = vim.fn.getcwd()
 local project_file = root .. '/project.lua'
 if vim.fn.filereadable(project_file) == 1 then
-    dofile(project_file)
+    local project = dofile(project_file)
+    if project.setup then
+        local terminal = require("coolcodersuper.terminal")
+        project.setup(terminal)
+    end
 end
